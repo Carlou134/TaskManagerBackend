11c4b91b66500ab77bf69bcb59337481e9326c07
Carlou134 <carlouvasquez134@gmail.com>
2026-01-10 03:31:52 -0500

    Migracion de archivos

--- TaskManagerBackend/Program.cs
+++ TaskManagerBackend/Program.cs
@@ -1,4 +1,32 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//options.UseNpgsql(
+//    builder.Configuration.GetConnectionString("TaskManager")
+//    )
+//);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["key"]!))
+        };
+    });
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
